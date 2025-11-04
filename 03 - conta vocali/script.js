e738bd6c2a8c219dbfb2a156723d941ae1e77038
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-04 14:18:59 +0100

    feat: completo conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,7 +5,26 @@
 const word = "javascript";
 
 // Dichiara la funzione qui.
+function numeriVocali(word) {
+  let number = 0;
+  for (let i = 0; i < word.length; i++) {
+    const vocale = word[i].toLowerCase();
+    if (
+      vocale === "a" ||
+      vocale === "e" ||
+      vocale === "i" ||
+      vocale === "o" ||
+      vocale === "u"
+    ) {
+      number++;
+    }
+  }
+  return number;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const risultato = numeriVocali(word);
+console.log(risultato);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
