3e174eb12803f0b4817e1e3422ba35c1ce0cfd7e
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-04 14:07:33 +0100

    feat:  completo filtra iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -6,7 +6,23 @@ con quella lettera */
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
+function filtraPerLettera(name, lettera) {
+  const paroleFiltrate = [];
+  const letteraMaiuscola = lettera.toUpperCase();
+
+  for (let i = 0; i < name.length; i++) {
+    const nome = name[i];
+    const iniziale = nome[0].toUpperCase();
+    if (iniziale === letteraMaiuscola) {
+      paroleFiltrate.push(nome);
+    }
+  }
+
+  return paroleFiltrate;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const risultato = filtraPerLettera(names, "A");
+console.log(risultato);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
