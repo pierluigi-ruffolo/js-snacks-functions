aca7c030e1442f0b32985fbe9f1225dfc7bdfb05
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-04 13:55:18 +0100

    feat: completo iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,29 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e 
+restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const names = [
+  "Anna",
+  "Luca",
+  "Marco",
+  "Adele",
+  "Giovanni",
+  "Alessandra",
+  "Giovanna",
+];
 
 // Dichiara la funzione qui.
-
+function iniziali(nomi) {
+  const inizialiArray = [];
+  for (let i = 0; i < nomi.length; i++) {
+    const nome = nomi[i];
+    const iniziale = nome[0];
+    inizialiArray.push(iniziale);
+  }
+  return inizialiArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const risultato = iniziali(names);
+console.log(risultato);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,11 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa
+ e restituisca il numero di vocali
+  contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
-
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
