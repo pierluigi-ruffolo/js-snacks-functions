9ef42196db72e72f997dc7204506b9efa16e281f
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-04 14:24:05 +0100

    feat:  completo saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,11 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un 
+nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
-
+const saluta = (name) => `Ciao ${name}`;
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+const risultato = saluta(name);
+console.log(risultato);
 //Risultato atteso se si passa 'Mario': // ciao Mario
